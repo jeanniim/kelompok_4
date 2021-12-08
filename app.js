03dc5383fd017039193e68bbf1008e4197c61b93
unknown <you@example.com>
2021-12-08 10:24:21 +0700

    add some notes

--- app.js
+++ app.js
@@ -157,10 +157,10 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
 	}
 
 
-
-
 //cycle over completedTasksHolder ul list items
 	for (var i=0; i<completedTasksHolder.children.length;i++){
 	//bind events to list items chldren(tasksIncompleted)
 		bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
 	}
+
+// Assignment Day 20
